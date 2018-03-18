83422ec43fb344ed8904e1c6b12356889fbdf59e
Fkeito <kei1229tomi@gmail.com>
2018-03-19 04:17:27 +0900

    フォルダ整理とプレイヤーと敵のベース作成

---
+++ VRShooting/Assets/Scripts/EnemyBase.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyBase : MonoBehaviour {
+
+    protected int point;
+
+    protected int _hp;
+    public int hp{ get{ return _hp; } }
+
+    public void Attacked(){
+        _hp--;
+
+        if(hp <= 0){
+            PlayerController.score += point;
+
+            Destroy(this.gameObject);
+        }
+    }
+
+}
---
+++ VRShooting/Assets/Scripts/GunShooter.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GunShooter : MonoBehaviour {
+
+    private SteamVR_TrackedObject trackedObject;
+    private SteamVR_Controller.Device device;
+
+    void Awake()
+    {
+        trackedObject = GetComponent<SteamVR_TrackedObject>();
+        device = SteamVR_Controller.Input((int)trackedObject.index);
+    }
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (device.GetPressDown(SteamVR_Controller.ButtonMask.Trigger))
+        {
+            Fire();
+        }
+	}
+
+    void Fire(){
+        Ray ray = new Ray(transform.position, transform.forward);
+
+        RaycastHit hit;
+        EnemyBase enemy = null;
+
+        if (Physics.Raycast(ray, out hit))
+        {
+            enemy = hit.collider.gameObject.GetComponent<EnemyBase>();
+        }
+
+        if(enemy){
+            enemy.Attacked();
+        }
+    }
+
+
+}
