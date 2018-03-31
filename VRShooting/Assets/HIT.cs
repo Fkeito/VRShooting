3703c39671ea81efb4560ae8f7565494adc61412
kotarohata <kotarohata1021@icloud.com>
2018-03-31 16:30:57 +0900

    ダメージ食らった時のSE追加

---
+++ VRShooting/Assets/HIT.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HIT : MonoBehaviour {
+
+    public AudioClip sound;
+
+    void OnCollisionEnter(Collision collision)
+    {
+        AudioSource.PlayClipAtPoint(sound, transform.position);
+    }
+}
