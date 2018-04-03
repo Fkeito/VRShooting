aed0fc03ed5d7a483100813dfc09aa35ce6f0d23
kotarohata <kotarohata1021@icloud.com>
2018-04-03 19:27:17 +0900

    盾の効果音、HPバーの色、playerの調整

---
+++ VRShooting/Assets/Change_Color.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Change_Color : MonoBehaviour
+{
+
+
+
+    private int flag = 1;
+    Slider playerslider1;
+    public Image playerslider2;
+
+
+    void Start()
+    {
+        playerslider1 = GameObject.Find("HP").GetComponent<Slider>();
+        playerslider2 = GameObject.Find("Fill").GetComponent<Image>();
+
+    }
+    void Update()
+    {
+        if (playerslider1.value <= 50 && flag == 1)
+        {
+
+            playerslider2.color = Color.yellow;
+            flag = flag - 1;
+        }
+
+        if (playerslider1.value <= 30 && flag == 0)
+        {
+            playerslider2.color = Color.red;
+            flag = flag - 1;
+        }
+
+    }
+}
