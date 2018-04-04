7ba666f7a5fc408b1519b5b6d5a8c8c77f2909de
Fkeito <1417089@ed.tus.ac.jp>
2018-04-04 22:27:00 +0900

    空飛ぶ敵試作

---
+++ VRShooting/Assets/Scripts/FighterController.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FighterController : EnemyBase
+{
+
+    private Vector3 bulletPos;
+
+    public GameObject bullet;
+    public GameObject explosion;
+
+    // Use this for initialization
+    void Start()
+    {
+        bulletPos = new Vector3(0, -0.2f, -1f);
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Fire();
+        }
+        if (Input.GetKeyDown(KeyCode.D))
+        {
+            Destroy(this.gameObject);
+        }
+    }
+
+    void OnDisable()
+    {
+        Dead();
+    }
+
+    void Fire()
+    {
+        GameObject obj = Instantiate(bullet);
+        obj.transform.position = bulletPos;
+        Rigidbody rb = obj.GetComponent<Rigidbody>();
+        rb.AddForce(obj.transform.forward * -100);
+
+        Destroy(obj, 5f);
+    }
+
+    void Dead()
+    {
+        GameObject obj = Instantiate(explosion);
+        Destroy(obj, 2f);
+    }
+
+}
